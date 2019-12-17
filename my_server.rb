0461ad6a14543b392878d80404e385248987181f
Hanna Lee <mani21na@gmail.com>
2019-12-17 00:43:17 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -1,9 +1,9 @@
 class MyServer
   def call(env)
-    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+    return [ 200, {'Content-Type' => 'text/html'}, last_response ]
   end
  
-  def pretty_response
+  def last_response
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 end
